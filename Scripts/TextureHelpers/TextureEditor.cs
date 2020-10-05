7812a493135edbf7b0d2340737738dac9b18ea9c
João Vitor Amorim <58463656+JV-Amorim@users.noreply.github.com>
2020-10-05 10:55:07 -0300

    RotateTexture() method added (fixing last commit).

--- Scripts/TextureHelpers/TextureEditor.cs
+++ Scripts/TextureHelpers/TextureEditor.cs
@@ -14,30 +14,30 @@ namespace UnityHelpers.TextureHelpers
 
             return newTexture;
         }
-    }
-
-    Texture2D RotateTexture(Texture2D originalTexture, bool clockwise)
-    {
-        Color32[] original = originalTexture.GetPixels32();
-        Color32[] rotated = new Color32[original.Length];
-        int w = originalTexture.width;
-        int h = originalTexture.height;
-
-        int iRotated, iOriginal;
-
-        for (int j = 0; j < h; ++j)
+        
+        Texture2D RotateTexture(Texture2D originalTexture, bool clockwise)
         {
-            for (int i = 0; i < w; ++i)
-            {
-                iRotated = (i + 1) * h - j - 1;
-                iOriginal = clockwise ? original.Length - 1 - (j * w + i) : j * w + i;
-                rotated[iRotated] = original[iOriginal];
-            }
-        }
+            Color32[] original = originalTexture.GetPixels32();
+            Color32[] rotated = new Color32[original.Length];
+            int w = originalTexture.width;
+            int h = originalTexture.height;
 
-        Texture2D rotatedTexture = new Texture2D(h, w);
-        rotatedTexture.SetPixels32(rotated);
-        rotatedTexture.Apply();
-        return rotatedTexture;
+            int iRotated, iOriginal;
+
+            for (int j = 0; j < h; ++j)
+            {
+                for (int i = 0; i < w; ++i)
+                {
+                    iRotated = (i + 1) * h - j - 1;
+                    iOriginal = clockwise ? original.Length - 1 - (j * w + i) : j * w + i;
+                    rotated[iRotated] = original[iOriginal];
+                }
+            }
+
+            Texture2D rotatedTexture = new Texture2D(h, w);
+            rotatedTexture.SetPixels32(rotated);
+            rotatedTexture.Apply();
+            return rotatedTexture;
+        }
     }
 }
